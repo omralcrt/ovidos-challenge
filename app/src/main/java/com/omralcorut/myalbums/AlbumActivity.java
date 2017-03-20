147cad1fd078b8da05c0be5bc5a00c2ed61acea1
Ömral Cörüt <omralcrt@gmail.com>
2017-03-20 22:33:52 +0300

    Create listView and add icon

--- app/src/main/java/com/omralcorut/myalbums/AlbumActivity.java
+++ app/src/main/java/com/omralcorut/myalbums/AlbumActivity.java
@@ -2,6 +2,10 @@ package com.omralcorut.myalbums;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AlbumActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class AlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+
+        //Create fake album arrayList
+        ArrayList<String> albums = new ArrayList<>();
+        albums.add("Album1");
+        albums.add("Album2");
+        albums.add("Album3");
+        albums.add("Album4");
+        albums.add("Album5");
+        albums.add("Album6");
+        albums.add("Album7");
+
+        //Initilize listView
+        ListView albumListView = (ListView) findViewById(R.id.list);
+
+        //Create array adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, albums
+        );
+
+        //connect albums arrayList to listView
+        albumListView.setAdapter(adapter);
+
     }
 }
