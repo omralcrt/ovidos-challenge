3429efa6f293c0f31f4e4709dc251fc7d66915f1
Ömral Cörüt <omralcrt@gmail.com>
2017-03-21 13:21:11 +0300

    Change gridview via cardView for photos

--- app/src/main/java/com/omralcorut/myalbums/PhotoAdapter.java
+++ app/src/main/java/com/omralcorut/myalbums/PhotoAdapter.java
@@ -1,11 +1,14 @@
 package com.omralcorut.myalbums;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -13,47 +16,57 @@ import java.util.List;
  * Created by omral on 21.03.2017.
  */
 
-public class PhotoAdapter extends ArrayAdapter<Photo> {
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.MyViewHolder> {
 
     private static final String LOG_TAG = PhotoAdapter.class.getName();
 
-    public PhotoAdapter(Context context, List<Photo> photos) {
-        super(context, 0, photos);
+    private Context mContext;
+    private List<Photo> photos;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        }
+    }
+
+
+    public PhotoAdapter(Context mContext, List<Photo> photos) {
+        this.mContext = mContext;
+        this.photos = photos;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.photo_list_item, parent, false);
 
-        //Check is there a listItem view
-        View listItemView = convertView;
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.photo_list_item, parent, false
-            );
-        }
+        return new MyViewHolder(itemView);
+    }
 
-        //Create a photo object
-        Photo currentPhoto = getItem(position);
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Photo photo = photos.get(position);
+        holder.title.setText(photo.getTitle());
 
-        //Display title of object
-        TextView title = (TextView) listItemView.findViewById(R.id.title);
-        title.setText(currentPhoto.getTitle().toUpperCase());
+        // loading album cover using Glide library
+        Glide.with(mContext).load(photo.getThumbnailUrl()).into(holder.thumbnail);
 
-        /*
-        //Display image of object
-        ImageView thumbnail = (ImageView) listItemView.findViewById(R.id.thumbnail);
-        Bitmap bmp = null;
-        try {
-            URL url = new URL(currentPhoto.getThumbnailUrl());
-            bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "Problem converting URL.", e);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem converting bitmap.", e);
-        }
-        thumbnail.setImageBitmap(bmp);
-        */
+    }
 
-        return listItemView;
+    @Override
+    public int getItemCount() {
+        return photos.size();
+    }
+
+    public void clear() {
+        int size = this.photos.size();
+        this.photos.clear();
+        notifyItemRangeRemoved(0, size);
     }
 }
+
